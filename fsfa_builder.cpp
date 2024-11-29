b98d326f4319b21569c44fa05b5b5c32860ba156
Lily Haverlag <flannyha@gmail.com>
2024-11-29 04:40:29 +0100

    Validate output when requested

--- fsfa_builder.cpp
+++ fsfa_builder.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <vector>
 
+bool verbose = false;
+
 struct Header {
     char file_magic[4];
     uint32_t n_items;
@@ -104,10 +106,59 @@ int traverse_directory(const std::filesystem::path input_path, std::vector<Item>
     return n_items_in_this_folder;
 }
 
-int main(int argc, char** argv) {
-    if (argc != 3) {
-        printf("Usage: fsfa_builder.exe <input path> <output file>");
+void visualize_file_structure(const Item* items_list, int index = 0, int depth = 0) {
+    printf("\n");
+    for (int i = 0; i < depth; ++i) printf(".   ");
+    const Item* item = &items_list[index];
+    char name[13] = {0};
+    char extension[4] = {0};
+    memset(name, 0, sizeof(name));
+    memset(extension, 0, sizeof(extension));
+
+    if (item->type == ItemType::File) {
+        strncpy(name, item->name, sizeof(name));
+        strncpy(extension, item->extension, sizeof(extension));
+        name[sizeof(name)-1] = 0;
+        extension[sizeof(extension)-1] = 0;
+        printf("File %s.%s", name, extension);
+        if (item->size <= 1024) printf(" - %i bytes", item->size);
+        else if (item->size <= 1024*1024) printf(" - %i KB", item->size / 1024);
+        else if (item->size <= 1024*1024*1024) printf(" - %i MB", item->size / (1024 * 1024));
+        else printf(" - %i GB", item->size / (1024 * 1024 * 1024));
     }
+
+    else if (item->type == ItemType::Folder) {
+        strncpy(name, item->name, sizeof(name));
+        name[sizeof(name)-1] = 0;
+        printf("Folder %s: (%i items, %i-%i)", name, item->size, item->offset, item->offset + item->size - 1);
+
+        for (uint32_t i = 0; i < item->size; ++i) {
+            strncpy(name, items_list[item->offset + i].name, sizeof(name));
+            strncpy(extension, item->extension, sizeof(extension));
+            name[sizeof(name)-1] = 0;
+            extension[sizeof(extension)-1] = 0;
+            visualize_file_structure(items_list, item->offset + i, depth + 1);
+        }
+    }
+
+    else {
+        throw std::runtime_error("Output file validation error: invalid item type");
+    }
+}
+
+bool find_argument(int argc, char** argv, const char* long_flag, const char* short_flag) {
+    for (int i = 1; i < argc; ++i) {
+        if (strncmp(argv[i], long_flag, strlen(long_flag)) == 0) return true;
+        if (strncmp(argv[i], short_flag, strlen(short_flag)) == 0) return true;
+    }
+    return false;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        printf("Usage: fsfa_builder.exe <input path> <output file> [--verbose/-v]");
+    }
+    verbose = find_argument(argc, argv, "--verbose", "-v");
 
     const char* input_path = argv[1];
     const char* output_path = argv[2];
@@ -141,6 +192,46 @@ int main(int argc, char** argv) {
     file.write((char*)items.data(), items_size);
     file.write((char*)binary_data.data(), data_size);
     file.close();
+
+    // validate
+    if (verbose) {
+        const auto filesize = std::filesystem::file_size(output_path);
+        char* contents = new char[filesize];
+        {
+            std::ifstream fsfa;
+            fsfa.open(output_path, std::ios_base::in | std::ios_base::binary);
+            fsfa.read(contents, filesize);
+            fsfa.close();
+
+            Header* header = (Header*)contents;
+            if (
+                header->file_magic[0] != 'F' ||
+                header->file_magic[1] != 'S' ||
+                header->file_magic[2] != 'F' ||
+                header->file_magic[3] != 'A'
+            ) {
+                throw std::runtime_error("Output file validation error: file magic invalid");
+            }
+
+            Item* items = (Item*)(&contents[sizeof(Header) + header->items_offset]);
+
+            if (
+                items[0].name[0] != 'r' ||
+                items[0].name[1] != 'o' ||
+                items[0].name[2] != 'o' ||
+                items[0].name[3] != 't'
+            ) {
+                throw std::runtime_error("Output file validation error: first node should be a folder named \"root\", which is not the case!");
+            }
+
+            visualize_file_structure(items);
+        }
+        delete[] contents;
+
+        printf("\n");
+    }
+
+    printf ("Packed folder \"%s\" into \"%s\"\n", input_path, output_path);
 
     return 0;
 }
