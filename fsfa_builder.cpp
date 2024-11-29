7ae89afa7cb9802cdb63984385d49f37f265b672
Lily Haverlag <flannyha@gmail.com>
2024-11-29 01:10:39 +0100

    Create items

--- fsfa_builder.cpp
+++ fsfa_builder.cpp
@@ -40,15 +40,17 @@ struct Item {
 };
 static_assert(sizeof(Item) == 24);
 
-std::vector<Item> items;
+void traverse_directory(std::filesystem::path input_path, std::vector<Item>& items, std::vector<uint8_t>& binary_data, const int depth) {
+    std::vector<std::pair<int, std::filesystem::path>> folders_to_parse; // [item_index, path]
+    std::vector<std::pair<int, std::filesystem::path>> files_to_parse; // [item_index, path]
 
-void traverse_directory(std::filesystem::path input_path, const int depth) {
     for (const auto& entry : std::filesystem::directory_iterator(input_path)) {
         if (!entry.is_regular_file() && !entry.is_directory()) {
             throw std::runtime_error("Item was neither file nor directory!");
         }
 
         if (entry.is_directory()) {
+            folders_to_parse.emplace_back(items.size(), entry.path());
             items.emplace_back(
                 ItemType::Folder,
                 entry.path().filename().string().c_str()
@@ -58,16 +60,18 @@ void traverse_directory(std::filesystem::path input_path, const int depth) {
         if (entry.is_regular_file()){
             auto name = entry.path().stem().string().c_str();
             auto extension = entry.path().extension().string().c_str();
+            files_to_parse.emplace_back(items.size(), entry.path());
             items.emplace_back(
                 ItemType::File,
                 name,
                 extension
             );
         }
+    }
 
-        if (entry.is_directory()) {
-            traverse_directory(entry.path(), depth + 1);
-        }
+    for (const auto& [index, path] : folders_to_parse) {
+    }
+    for (const auto& [index, path] : files_to_parse) {
     }
 }
 
@@ -78,8 +82,11 @@ int main(int argc, char** argv) {
 
     const char* input_path = argv[1];
     const char* output_path = argv[2];
+    std::vector<Item> items;
+    std::vector<uint8_t> binary_data;
+    traverse_directory(input_path, items, binary_data, 0);
 
-    traverse_directory(input_path, 0);
+
 
     return 0;
 }
